8bc938fdcfbfa0c904c97539f5e133090e610eed
EkaMakhsusan <ekamahsusan@gmail.com>
2024-09-01 12:23:10 +0700

    Add files via upload
    Tugas Praktikum Pemrograman Berorientasi Objek Pertemuan 2

---
+++ Ekaproject1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ekaproject1;
+
+/**
+ *
+ * @author HP
+ */
+public class Ekaproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Nama : Eka Makhsusan");
+        System.out.println("NPM : 2320506042");
+        System.out.println("Alamat : Temanggung");
+        System.out.println("No_HP : 088112233445");
+    }
+}
