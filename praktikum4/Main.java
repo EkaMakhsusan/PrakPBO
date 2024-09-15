75922023cf87eddb8eb20fa36a916d5f7f81f907
EkaMakhsusan <ekamahsusan@gmail.com>
2024-09-15 19:21:40 +0700

    Add files via upload
    Praktikum Pemrograman Berorientasi Objek Pertemuan 4

---
+++ praktikum4/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum4;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+    public static void main(String[] args) {
+        Mobil mobil = new Mobil("Toyota", 180, "Bensin", 4);
+
+        mobil.tampilkanInfoKendaraan();
+        mobil.tampilkanInfoMobil();
+     }
+}
